fb52beb2f80ac2e34bbbdc602c921108995ec3e1
Gautam Makwana <139319282+gautammakwana1@users.noreply.github.com>
2023-10-06 20:41:08 +0530

    06/10

---
+++ maths.js
@@ -0,0 +1,260 @@
+//  Maths Properties -> this is constant value (fixe value)
+
+console.log(`
+
+Maths Properties :-
+
+
+`)
+
+console.log("Euler's value : ",Math.E);
+console.log("PI's value : ",Math.PI);
+console.log("Square Root of 2 value :",Math.SQRT2);
+console.log("Square Root of 1/2 value :",Math.SQRT1_2);
+console.log("Natural logarithm of 2 value :",Math.LN2);
+console.log("Natural logarithm of 10 value :",Math.LN10);
+console.log("Base 2 logarithm of E value :",Math.LOG2E);
+console.log("Base 10 logarithm of E  value :",Math.LOG10E);
+
+
+//  Maths Method 
+console.log(`
+
+Math's Method  :-
+
+
+`);
+
+console.log("Round Method :")
+
+console.log(Math.round(15.5));      // 16
+console.log(Math.round(-15.5));     //-15
+console.log(Math.round(0.6));       //1
+console.log(Math.round(-0.6));      //-1
+
+console.log("Ceil Method :");
+
+console.log(Math.ceil(5.2));        //6
+console.log(Math.ceil(0.1));        //1
+console.log(Math.ceil(-0.1));       //-0
+console.log(Math.ceil(0.9));        //1
+
+console.log("Floor Method :");
+
+console.log(Math.floor(4.9));
+console.log(Math.floor(-4.4));
+console.log(Math.floor(0.5));
+console.log(Math.floor(-0.1));
+
+console.log("Random Method :");
+
+console.log(Math.random());        // always between 0 to 1
+// want between 1 to 100
+console.log(Math.random()*10);     //multiple 10 with random function
+console.log(Math.random()*100);     //multiple 100 with random function
+console.log(Math.random()*1000);        //multiple 1000 with random function
+
+console.log("Trunc Method :");
+
+console.log(Math.trunc(4.6));       //4
+console.log(Math.trunc(-8.1));      //-8
+console.log(Math.trunc(-0.16));     //-0
+console.log(Math.trunc(0.097));     //0
+
+console.log("Power Method :");
+
+console.log(Math.pow(2,5));
+console.log(Math.pow(2,5));
+//  Maths Properties -> this is constant value
+
+console.log("Euler's value : ",Math.E);
+console.log("PI's value : ",Math.PI);
+console.log("Square Root of 2 value :",Math.SQRT2);
+console.log("Square Root of 1/2 value :",Math.SQRT1_2);
+console.log("Natural logarithm of 2 value :",Math.LN2);
+console.log("Natural logarithm of 10 value :",Math.LN10);
+console.log("Base 2 logarithm of E value :",Math.LOG2E);
+console.log("Base 10 logarithm of E  value :",Math.LOG10E);
+
+
+//  Maths Method 
+console.log(`
+
+Math's Method  :-
+
+
+`);
+
+console.log("Round Method :")
+
+console.log(Math.round(15.5));      // 16
+console.log(Math.round(-15.5));     //-15
+console.log(Math.round(0.6));       //1
+console.log(Math.round(-0.6));      //-1
+
+console.log("Ceil Method :");
+
+console.log(Math.ceil(5.2));        //6
+console.log(Math.ceil(0.1));        //1
+console.log(Math.ceil(-0.1));       //-0
+console.log(Math.ceil(0.9));        //1
+
+console.log("Floor Method :");
+
+console.log(Math.floor(4.9));
+console.log(Math.floor(-4.4));
+console.log(Math.floor(0.5));
+console.log(Math.floor(-0.1));
+
+console.log("Random Method :");
+
+console.log(Math.random());        // always between 0 to 1
+// want between 1 to 100
+console.log(Math.random()*10);     //multiple 10 with random function
+console.log(Math.random()*100);     //multiple 100 with random function
+console.log(Math.random()*1000);        //multiple 1000 with random function
+
+console.log("Trunc Method :");
+
+console.log(Math.trunc(4.6));       //4
+console.log(Math.trunc(-8.1));      //-8
+console.log(Math.trunc(-0.16));     //-0
+console.log(Math.trunc(0.097));     //0
+
+console.log("Power Method :");
+
+console.log(Math.pow(2,5));
+console.log(Math.pow(2,5));
+//  Maths Properties -> this is constant value
+
+console.log("Euler's value : ",Math.E);
+console.log("PI's value : ",Math.PI);
+console.log("Square Root of 2 value :",Math.SQRT2);
+console.log("Square Root of 1/2 value :",Math.SQRT1_2);
+console.log("Natural logarithm of 2 value :",Math.LN2);
+console.log("Natural logarithm of 10 value :",Math.LN10);
+console.log("Base 2 logarithm of E value :",Math.LOG2E);
+console.log("Base 10 logarithm of E  value :",Math.LOG10E);
+
+
+//  Maths Method 
+console.log(`
+
+Math's Method  :-
+
+
+`);
+
+console.log("Round Method :")
+
+console.log(Math.round(15.5));      // 16
+console.log(Math.round(-15.5));     //-15
+console.log(Math.round(0.6));       //1
+console.log(Math.round(-0.6));      //-1
+
+console.log("Ceil Method :");
+
+console.log(Math.ceil(5.2));        //6
+console.log(Math.ceil(0.1));        //1
+console.log(Math.ceil(-0.1));       //-0
+console.log(Math.ceil(0.9));        //1
+
+console.log("Floor Method :");
+
+console.log(Math.floor(4.9));
+console.log(Math.floor(-4.4));
+console.log(Math.floor(0.5));
+console.log(Math.floor(-0.1));
+
+console.log("Random Method :");
+
+console.log(Math.random());        // always between 0 to 1
+// want between 1 to 100
+console.log(Math.random()*10);     //multiple 10 with random function
+console.log(Math.random()*100);     //multiple 100 with random function
+console.log(Math.random()*1000);        //multiple 1000 with random function
+
+console.log("Trunc Method :");
+
+console.log(Math.trunc(4.6));       //4
+console.log(Math.trunc(-8.1));      //-8
+console.log(Math.trunc(-0.16));     //-0
+console.log(Math.trunc(0.097));     //0
+
+console.log("Power Method :");
+
+console.log(Math.pow(2,5));     // 2*2*2*2*2
+console.log(Math.pow(12,2));    //144
+console.log(Math.pow(10,2));    //100
+console.log(Math.pow(5,3));     //125
+
+console.log("Sign Method :");
+//                                it gives positive = 1, negative = -1,positive 0 = 0,negative 0 = -0  
+console.log(Math.sign(5.16));       //1
+console.log(Math.sign(-12.16));     //-1
+console.log(Math.sign(0));          //0
+console.log(Math.sign(-0));         //-0
+
+console.log("Absolute Method :");
+//                                  it gives positive value
+console.log(Math.abs(5.56));
+console.log(Math.abs(-12.75));
+console.log(Math.abs(-0.56));
+console.log(Math.abs(0.10));
+
+console.log("Squar Root Method :");
+
+console.log(Math.sqrt(2));
+console.log(Math.sqrt(4));
+console.log(Math.sqrt(12));
+console.log(Math.sqrt(81));
+
+console.log("Minimum Value Method :");
+
+console.log(Math.min(10,2,50,42,99,155,-1,-50,1500));       //-50
+
+console.log("Maximum Value Method :");
+
+console.log(Math.max(10,2,50,42,99,155,-1,-50,1500));       //1500
+
+console.log("Sin Method :");
+            //Eqvation = degree *Math.PI /180;      0 to 1 
+console.log(Math.sin(30*Math.PI/180));     //0.4999.......
+console.log(Math.sin(90*Math.PI/180));     //1
+console.log(Math.sin(160*Math.PI/180));     //0.3420.......
+console.log(Math.sin(170*Math.PI/180));     //0.1736.......
+
+console.log("Cos Method :");
+            //Eqvation = degree *Math.PI /180;     1 to -1
+console.log(Math.cos(30*Math.PI/180));      //0.8660.........
+console.log(Math.cos(0*Math.PI/180));      //0.8660.........
+console.log(Math.cos(180*Math.PI/180));      //0.8660.........
+console.log(Math.cos(120*Math.PI/180));      //0.8660.........
+
+console.log("Tan Method :");
+            //Eqvation = degree *Math.PI /180;
+console.log(Math.tan(30*Math.PI/180));         //0.5773......
+console.log(Math.tan(0*Math.PI/180));          //0
+console.log(Math.tan(110*Math.PI/180));        //-2.747.......
+console.log(Math.tan(380*Math.PI/180));        //0.3639.......
+
+console.log("Natural Logartithm Method :");
+
+console.log(Math.log(2));       //0.6931......
+console.log(Math.log(10));       //2.3025......
+console.log(Math.log(35));       //3.5553......
+console.log(Math.log(5.5));       //1.7047......
+
+console.log("Base 2 Logartithm Method :");
+
+console.log(Math.log2(2));      //1
+console.log(Math.log2(10));      //3.3219.......
+console.log(Math.log2(35));      //5.1292.....
+console.log(Math.log2(5.5));      //2.4594.....
+
+console.log("Base 10 Logartithm Method :");
+
+console.log(Math.log10(2));        //0.3010.....
+console.log(Math.log10(10));        //1
+console.log(Math.log10(35));        //1.5440.....
+console.log(Math.log10(5.5));        //0.7403.....
