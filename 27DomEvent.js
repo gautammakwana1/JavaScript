93f01261ac3e86714fa456326143ea0c0411c1a3
Gautam Makwana <139319282+gautammakwana1@users.noreply.github.com>
2023-10-24 18:47:25 +0530

    24/10

---
+++ 27DomEvent.js
@@ -0,0 +1,56 @@
+/*  JavaScript HTML DOM Events */
+
+// function message(){
+//     alert("Yes, I Am Heading Tag")
+// }
+
+// function dateshow(){
+//     document.getElementById("date").innerHTML = Date();
+// }
+
+// function uppercase(){
+//     const change = document.getElementById("emails"); 
+//     change.value = change.value.toUpperCase();
+// }
+
+// function red(one){
+//     one.style.color= "yellow";
+// }
+
+// {
+//     function Down(obj) {
+//         obj.style.backgroundColor = "#1ec5e5";
+//         obj.innerHTML = "Release Me";
+//       }
+      
+//       function Up(obj) {
+//         obj.style.backgroundColor="#D94A38";
+//         obj.innerHTML="Thank You";
+//       }
+// }
+
+/* JavaScript addEventListener() */
+
+{
+    // const x = document.getElementById("clickIt");
+    //     const y = document.getElementById("hoverPara");
+          
+    //     x.addEventListener("click", RespondClick);
+    //     y.addEventListener("mouseover", RespondMouseOver);
+    //     y.addEventListener("mouseout", RespondMouseOut);
+          
+    //     function RespondMouseOver() {
+    //         document.getElementById("effect").innerHTML +=
+    //                    "Aee Tu fir aa gya yha jaher khane k paise nhi hai re...." + "<br>";
+    //     }
+          
+    //     function RespondMouseOut() {
+    //         document.getElementById("effect").innerHTML +=
+    //                   "Khopari Tod sale ka...." + "<br>";
+    //     }
+          
+    //     function RespondClick() {
+    //         document.getElementById("effect").innerHTML +=
+    //                   "Yee Babu Rao ka Style hai re...." + "<br>";
+    //     }
+}
